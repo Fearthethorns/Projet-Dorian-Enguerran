ec98f7df69b9cfb71f2c6690ce6bfce4722d5801
Enguerran Guinhouya <enguerran.guinhouya@univ-tlse3.fr>
2018-11-14 17:35:54 +0000

    polonaise

---
+++ polonaise.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#define CAPACITE 10000
+#define TAILLE 10
+
+void pousser(char tableau[]);
+    int x;
+    tableau[0]=x;
+    tableau[0]=tableau[sizeof(tableau)];
+    tableau[sizeof(tableau)]=x;
+
+int main(int argc,char*argv[]){
+    char sysraw[];
+    scanf("%s",sysraw);
+    int taille=sizeof(sysraw);
+    int compteur=0
+    int elementgauche=0;
+    int elementdroite=0;
+    for(compteur,compteur<taille,compteur++){
+        if('sysraw[compteur]'=='+'){
+            
+        }
+    }
+}
